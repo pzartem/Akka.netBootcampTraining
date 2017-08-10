d2eca6d35470420324eb0a69794fa940e073cec3
Artem Puznanov <pzartem@gmail.com>
2017-08-10 10:38:42 +0300

    Added Unit 1 and 3

---
+++ Unit2/Main.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ChartApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.sysChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.buttonAddSeries = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.sysChart)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// sysChart
+			// 
+			chartArea2.Name = "ChartArea1";
+			this.sysChart.ChartAreas.Add(chartArea2);
+			this.sysChart.Dock = System.Windows.Forms.DockStyle.Fill;
+			legend2.Name = "Legend1";
+			this.sysChart.Legends.Add(legend2);
+			this.sysChart.Location = new System.Drawing.Point(0, 0);
+			this.sysChart.Name = "sysChart";
+			series2.ChartArea = "ChartArea1";
+			series2.Legend = "Legend1";
+			series2.Name = "Series1";
+			this.sysChart.Series.Add(series2);
+			this.sysChart.Size = new System.Drawing.Size(684, 446);
+			this.sysChart.TabIndex = 0;
+			this.sysChart.Text = "sysChart";
+			// 
+			// buttonAddSeries
+			// 
+			this.buttonAddSeries.Location = new System.Drawing.Point(569, 390);
+			this.buttonAddSeries.Name = "buttonAddSeries";
+			this.buttonAddSeries.Size = new System.Drawing.Size(103, 31);
+			this.buttonAddSeries.TabIndex = 1;
+			this.buttonAddSeries.Text = "Add Series";
+			this.buttonAddSeries.UseVisualStyleBackColor = true;
+			this.buttonAddSeries.Click += new System.EventHandler(this.buttonAddSeries_Click);
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(684, 446);
+			this.Controls.Add(this.buttonAddSeries);
+			this.Controls.Add(this.sysChart);
+			this.Name = "Main";
+			this.Text = "System Metrics";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+			this.Load += new System.EventHandler(this.Main_Load);
+			((System.ComponentModel.ISupportInitialize)(this.sysChart)).EndInit();
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart sysChart;
+		private System.Windows.Forms.Button buttonAddSeries;
+	}
+}
+
