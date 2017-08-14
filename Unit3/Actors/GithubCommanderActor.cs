d0ae58a7001974c170d02290d20d2599941d4168
Artem Puzanov <Artem_Puzanov@epam.com>
2017-08-14 16:51:47 +0300

    Completed Unit-3

--- Unit3/Actors/GithubCommanderActor.cs
+++ Unit3/Actors/GithubCommanderActor.cs
@@ -8,7 +8,7 @@ namespace GithubActors.Actors
     /// <summary>
     /// Top-level actor responsible for coordinating and launching repo-processing jobs
     /// </summary>
-    public class GithubCommanderActor : ReceiveActor
+    public class GithubCommanderActor : ReceiveActor, IWithUnboundedStash
     {
         #region Message classes
 
@@ -46,39 +46,89 @@ namespace GithubActors.Actors
 
         private IActorRef _coordinator;
         private IActorRef _canAcceptJobSender;
+	    private RepoKey _repoJob;
+		public IStash Stash { get; set; }
 
-        public GithubCommanderActor()
-        {
-            Receive<CanAcceptJob>(job =>
-            {
-                _canAcceptJobSender = Sender;
-                _coordinator.Tell(job);
-            });
+	    private int pendingJobReplies;
 
-            Receive<UnableToAcceptJob>(job =>
-            {
-                _canAcceptJobSender.Tell(job);
-            });
+		public GithubCommanderActor()
+	    {
+		    Ready();
+	    }
 
-            Receive<AbleToAcceptJob>(job =>
-            {
-                _canAcceptJobSender.Tell(job);
+	    private void Ready()
+	    {
+		    Receive<CanAcceptJob>(job =>
+		    {
+			    _coordinator.Tell(job);
+			    _repoJob = job.Repo;
+			    BecomeAsking();
+		    });
+	    }
 
-                //start processing messages
-                _coordinator.Tell(new GithubCoordinatorActor.BeginJob(job.Repo));
+	    private void BecomeAsking()
+	    {
+		    _canAcceptJobSender = Sender;
+		    // block, but ask the router for the number of routees. Avoids magic numbers.
+		    pendingJobReplies = _coordinator.Ask<Routees>(new GetRoutees())
+			    .Result.Members.Count();
+		    Become(Asking);
 
-                //launch the new window to view results of the processing
-                Context.ActorSelection(ActorPaths.MainFormActor.Path).Tell(new MainFormActor.LaunchRepoResultsWindow(job.Repo, Sender));
-            });
-        }
+		    // send ourselves a ReceiveTimeout message if no message within 3 seonds
+		    Context.SetReceiveTimeout(TimeSpan.FromSeconds(3));
+	    }
 
-        protected override void PreStart()
-        {
-            _coordinator = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()), ActorPaths.GithubCoordinatorActor.Name);
-            base.PreStart();
-        }
+		private void Asking()
+	    {
+		    // stash any subsequent requests
+		    Receive<CanAcceptJob>(job => Stash.Stash());
 
-        protected override void PreRestart(Exception reason, object message)
+		    Receive<UnableToAcceptJob>(job =>
+		    {
+			    pendingJobReplies--;
+			    if (pendingJobReplies == 0)
+			    {
+				    _canAcceptJobSender.Tell(job);
+				    BecomeReady();
+			    }
+		    });
+
+		    Receive<AbleToAcceptJob>(job =>
+		    {
+			    _canAcceptJobSender.Tell(job);
+
+			    // start processing messages
+			    Sender.Tell(new GithubCoordinatorActor.BeginJob(job.Repo));
+
+			    // launch the new window to view results of the processing
+			    Context.ActorSelection(ActorPaths.MainFormActor.Path).Tell(
+				    new MainFormActor.LaunchRepoResultsWindow(job.Repo, Sender));
+
+			    BecomeReady();
+		    });
+	    }
+
+	    private void BecomeReady()
+	    {
+		    Become(Ready);
+		    Stash.UnstashAll();
+
+		    // cancel ReceiveTimeout
+		    Context.SetReceiveTimeout(null);
+	    }
+
+		protected override void PreStart()
+	    {
+		    // create a broadcast router who will ask all 
+		    // of them if they're available for work
+		    _coordinator =
+			    Context.ActorOf(Props.Create(() => new GithubCoordinatorActor())
+					    .WithRouter(FromConfig.Instance),
+				    ActorPaths.GithubCoordinatorActor.Name);
+		    base.PreStart();
+	    }
+
+		protected override void PreRestart(Exception reason, object message)
         {
             //kill off the old coordinator so we can recreate it from scratch
             _coordinator.Tell(PoisonPill.Instance);
--- Unit3/Actors/GithubWorkerActor.cs
+++ Unit3/Actors/GithubWorkerActor.cs
@@ -6,108 +6,108 @@ using Octokit;
 
 namespace GithubActors.Actors
 {
-    /// <summary>
-    /// Individual actor responsible for querying the Github API
-    /// </summary>
-    public class GithubWorkerActor : ReceiveActor
-    {
-        #region Message classes
+	/// <summary>
+	/// Individual actor responsible for querying the Github API
+	/// </summary>
+	public class GithubWorkerActor : ReceiveActor
+	{
+		#region Message classes
 
-        public class QueryStarrers
-        {
-            public QueryStarrers(RepoKey key)
-            {
-                Key = key;
-            }
+		public class QueryStarrers
+		{
+			public QueryStarrers(RepoKey key)
+			{
+				Key = key;
+			}
 
-            public RepoKey Key { get; private set; }
-        }
+			public RepoKey Key { get; private set; }
+		}
 
-        /// <summary>
-        /// Query an individual starrer
-        /// </summary>
-        public class QueryStarrer
-        {
-            public QueryStarrer(string login)
-            {
-                Login = login;
-            }
+		/// <summary>
+		/// Query an individual starrer
+		/// </summary>
+		public class QueryStarrer
+		{
+			public QueryStarrer(string login)
+			{
+				Login = login;
+			}
 
-            public string Login { get; private set; }
-        }
+			public string Login { get; private set; }
+		}
 
-        public class StarredReposForUser
-        {
-            public StarredReposForUser(string login, IEnumerable<Repository> repos)
-            {
-                Repos = repos;
-                Login = login;
-            }
+		public class StarredReposForUser
+		{
+			public StarredReposForUser(string login, IEnumerable<Repository> repos)
+			{
+				Repos = repos;
+				Login = login;
+			}
 
-            public string Login { get; private set; }
+			public string Login { get; private set; }
 
-            public IEnumerable<Repository> Repos { get; private set; }
-        }
+			public IEnumerable<Repository> Repos { get; private set; }
+		}
 
-        #endregion
+		#endregion
 
-        private IGitHubClient _gitHubClient;
-        private readonly Func<IGitHubClient> _gitHubClientFactory;
+		private IGitHubClient _gitHubClient;
+		private readonly Func<IGitHubClient> _gitHubClientFactory;
 
-        public GithubWorkerActor(Func<IGitHubClient> gitHubClientFactory)
-        {
-            _gitHubClientFactory = gitHubClientFactory;
-            InitialReceives();
-        }
+		public GithubWorkerActor(Func<IGitHubClient> gitHubClientFactory)
+		{
+			_gitHubClientFactory = gitHubClientFactory;
+			InitialReceives();
+		}
 
-        protected override void PreStart()
-        {
-            _gitHubClient = _gitHubClientFactory();
-        }
+		protected override void PreStart()
+		{
+			_gitHubClient = _gitHubClientFactory();
+		}
 
-        private void InitialReceives()
-        {
-            //query an individual starrer
-            Receive<RetryableQuery>(query => query.Query is QueryStarrer, query =>
-            {
-                // ReSharper disable once PossibleNullReferenceException (we know from the previous IS statement that this is not null)
-                var starrer = (query.Query as QueryStarrer).Login;
-                try
-                {
-                    var getStarrer = _gitHubClient.Activity.Starring.GetAllForUser(starrer);
+		private void InitialReceives()
+		{
+			// query an individual starrer
+			Receive<RetryableQuery>(query => query.Query is QueryStarrer, query =>
+			{
+		// ReSharper disable once PossibleNullReferenceException
+		// (we know from the previous IS statement that this is not null)
+		var starrer = (query.Query as QueryStarrer).Login;
 
-                    //ewww
-                    getStarrer.Wait();
-                    var starredRepos = getStarrer.Result;
-                    Sender.Tell(new StarredReposForUser(starrer, starredRepos));
-                }
-                catch (Exception ex)
-                {
-                    //operation failed - let the parent know
-                    Sender.Tell(query.NextTry());
-                }
-            });
+		// close over the Sender in an instance variable
+		var sender = Sender;
+				_gitHubClient.Activity.Starring.GetAllForUser(starrer)
+				  .ContinueWith<object>(tr =>
+				  {
+			  // query faulted
+			  if (tr.IsFaulted || tr.IsCanceled)
+						  return query.NextTry();
+			  // query succeeded
+			  return new StarredReposForUser(starrer, tr.Result);
+				  }).PipeTo(sender);
 
-            //query all starrers for a repository
-            Receive<RetryableQuery>(query => query.Query is QueryStarrers, query =>
-            {
-                // ReSharper disable once PossibleNullReferenceException (we know from the previous IS statement that this is not null)
-                var starrers = (query.Query as QueryStarrers).Key;
-                try
-                {
-                    var getStars = _gitHubClient.Activity.Starring.GetAllStargazers(starrers.Owner, starrers.Repo);
+			});
 
-                    //ewww
-                    getStars.Wait();
-                    var stars = getStars.Result;
-                    Sender.Tell(stars.ToArray());
-                }
-                catch (Exception ex)
-                {
-                    //operation failed - let the parent know
-                    Sender.Tell(query.NextTry());
-                }
-            });
-        }
-    }
+			// query all starrers for a repository
+			Receive<RetryableQuery>(query => query.Query is QueryStarrers, query =>
+			{
+		// ReSharper disable once PossibleNullReferenceException
+		// (we know from the previous IS statement that this is not null)
+		var starrers = (query.Query as QueryStarrers).Key;
+
+
+		// close over the Sender in an instance variable
+		var sender = Sender;
+				_gitHubClient.Activity.Starring.GetAllStargazers(starrers.Owner, starrers.Repo)
+					.ContinueWith<object>(tr =>
+					{
+				// query faulted
+				if (tr.IsFaulted || tr.IsCanceled)
+							return query.NextTry();
+						return tr.Result.ToArray();
+					}).PipeTo(sender);
+
+			});
+		}
+	}
 }
